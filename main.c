a19e4d2b2799c5d24dec2dfbe3c0e7e4a617b0a3
avionipevaju <antic.nikola@outlook.com>
2018-04-02 02:13:04 +0200

    Continued implementation with predecessor_timeout

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 typedef struct node {
     int waiting;
     struct node* next;
+    int predecessor_timeout;
 
 }node;
 
@@ -17,21 +18,31 @@ int lock_n_threads_with_timeout(int id, int* local, double timeout){
     double start_time = lrk_get_time();
     node *currentNode = malloc(sizeof(node));
     currentNode->next = NULL;
+    currentNode->predecessor_timeout = false;
     node *previous = lrk_get_and_set(&tail, currentNode);
-    if (previous != NULL ) { //queue isn't empty
+    if (previous == NULL) { // lock was free, acquire.
+        currentNode->waiting = false;
+        local[0] = currentNode;
+        return 1;
+    } else { //queue isn't empty
         currentNode->waiting = true;
         previous->next = currentNode;
-        local[0] = currentNode;
         while (currentNode->waiting) {
-            if(lrk_get_time() - start_time >= timeout){
-                previous->next = currentNode->next;
+            if(lrk_get_time() - start_time > timeout){
+                if(currentNode->next != NULL)
+                    lrk_get_and_set(&currentNode->next->predecessor_timeout, true);
+                while (lrk_compare_and_set(&currentNode->predecessor_timeout, true, true)){
+                    lrk_sleep(0);
+                }
+                lrk_get_and_set(&previous->next, currentNode->next);
+                if(currentNode->next != NULL)
+                    lrk_get_and_set(&currentNode->next->predecessor_timeout, false);
+                local[0]= currentNode;
                 return 0;
             }
-            lrk_sleep(1);
+            lrk_sleep(0);
         }
-        return 1;
-    } else {
-        currentNode->waiting = false; // lock was free, acquire.
+        currentNode->waiting = false;
         local[0] = currentNode;
         return 1;
     }
@@ -44,14 +55,15 @@ void unlock_n_threads_with_timeout(int id, int* local) {
             return;
         }
         while (currentNode->next == NULL) {
-            lrk_sleep(1);
+            lrk_sleep(0);
         }
     }
-    currentNode->next->waiting = false; //free lock, tell next node to acquire
+    currentNode->next->waiting = false;
 };
 
 int main() {
     printf("Start MCSLock!\n");
-    start_timeout_mutex_n_threads_test(0.1);
+    //0.0348
+    start_timeout_mutex_n_threads_test(0.05);
     return 0;
 }
